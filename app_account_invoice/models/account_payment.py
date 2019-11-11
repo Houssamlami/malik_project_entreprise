6394f1886db966564dbd602570336b934f5bf37e
Houssam <lmai.houssam@gmail.com>
2019-11-11 15:36:36 +0100

    ENHANCEMENT OF COMMIT "amount of payment V/C + Colis/Unités in BL"

--- app_account_invoice/models/account_payment.py
+++ app_account_invoice/models/account_payment.py
@@ -14,6 +14,8 @@ class AccountPayment(models.Model):
     charcuterie = fields.Boolean(string="Charcuterie", default=False)
     volaille = fields.Boolean(string="Volaille", default=False)
     note_payment = fields.Text(string="Remarque")
+    amount_charcuterie = fields.Float(string="Montant Charcuterie")
+    amount_volaille = fields.Float(string="Montant Volaille")
     
     
     @api.one
