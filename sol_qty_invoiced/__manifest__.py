e5120345fccc19109ace1d0afde4fcae1efc883c
Houssam <lmai.houssam@gmail.com>
2020-02-22 18:54:29 +0100

    sol update qty invoiced

---
+++ sol_qty_invoiced/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2016-2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Invoice line qty invoiced',
+    'summary': 'Account Invoice line qty invoiced',
+    'author': 'FHS Solutions',
+    'license': 'AGPL-3',
+    'website': '',
+    'category': 'Account',
+    'version': '11.0.1.0.0',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'wizard/sol_views.xml',
+    ],
+    'installable': True,
+}
