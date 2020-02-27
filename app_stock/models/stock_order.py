f018322d787b3ac1ff5733f3d8cd7a76a9e9948e
Houssam <lmai.houssam@gmail.com>
2020-02-27 17:26:15 +0100

    Expediteur in stock picking

--- app_stock/models/stock_order.py
+++ app_stock/models/stock_order.py
@@ -29,6 +29,7 @@ class StockPicking(models.Model):
     is_return_picking = fields.Boolean(string="Is Retour", compute='get_is_return_picking')
     name_provisoir = fields.Char(string="Nom Provisoir", compute='get_is_return_picking')
     number_product_to_deliver = fields.Float(string='Produits à livrer', compute='_compute_number_product_to_deliver')
+    expediteur_in_picking = fields.Selection([('MV', 'Malik V'), ('An', 'Atlas N')], related='sale_id.Expediteur', string="Expediteur")
     
     @api.one      
     def _compute_number_product_to_deliver(self):
