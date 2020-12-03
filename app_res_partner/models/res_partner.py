577585f63dd36df8ee6283ef0be301b064ce8555
Admin <Admin@DESKTOP-VHKQN61>
2020-12-03 09:27:19 +0000

    correction

--- app_res_partner/models/res_partner.py
+++ app_res_partner/models/res_partner.py
@@ -100,9 +100,7 @@ class ResPartner(models.Model):
     date_reblockage = fields.Date(string='Date de reblocage')
     date_actualy2 = fields.Date(string='today actualy2', compute='get_date')
     client_avs = fields.Boolean(string=u"Client AVS")
-
-    
-    
+ 
     _sql_constraints = [
         ('ref_unique_part', 'unique(ref)', 'La reference client doit etre unique!'),
     ]
