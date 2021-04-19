70bd083720adf5a0cd5b9081ed67f67b06ad553a
Admin <Admin@DESKTOP-VHKQN61>
2021-04-19 14:47:22 +0000

    access to a field on another module

--- app_sale_order/models/sale_order.py
+++ app_sale_order/models/sale_order.py
@@ -239,8 +239,8 @@ class SaleOrder(models.Model):
             if record.partner_id.bloque_ch and record.partner_id.bloque_vo:
                 record.test_bloque="Votre Client est bloqué"
             if record.partner_id.nbr_jours_decheance_charcuterie > record.partner_id.echeance_charcuterie_par_jour:
-                for x in self.res_partner:
-                    x.write({'bloque_ch': True})
+                partner = self.env['res.partner'].search([('id', '=', record.partner_id.id)])
+                partner.bloque_ch=True
             '''if record.cmd_charcuterie==True and record.cmd_volaille==False:
                 if record.partner_id.Client_Charcuterie: 
                     if record.partner_id.credit_charcuterie > record.partner_id.limite_credit_charcuterie: 
@@ -416,12 +416,13 @@ class SaleOrder(models.Model):
                         return {
                                 'warning': {'title': _('Error'), 'message': _('Error message'),},
                         }
-        if self.partner_id.nbr_jours_decheance_charcuterie > self.partner_id.echeance_charcuterie_par_jour:
+        if self.partner_id.blocagex_echeance_facture_charcuterie==True:                
+            if self.partner_id.nbr_jours_decheance_charcuterie > self.partner_id.echeance_charcuterie_par_jour:
                            # record.cmd_excep=self.env['res.partner'].search([('id', '=', sales.partner_id.id)]).bloque_vo
-            raise exceptions.ValidationError(_('Votre Client est bloqué ,  merci de  procéder au réglement!'))
-            return {
+                raise exceptions.ValidationError(_('Votre Client est bloqué ,  merci de  procéder au réglement!'))
+                return {
                                 'warning': {'title': _('Error'), 'message': _('Error message'),},
-            }
+                }
             
         if self._get_forbidden_state_confirm() & set(self.mapped('state')):
             raise UserError(_(
