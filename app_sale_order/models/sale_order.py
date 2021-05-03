cbe3216f2aae0612e0c086ba4e43e193faf3d1b2
Admin <Admin@DESKTOP-VHKQN61>
2021-05-03 15:44:26 +0000

    test function

--- app_sale_order/models/sale_order.py
+++ app_sale_order/models/sale_order.py
@@ -306,13 +306,13 @@ class SaleOrder(models.Model):
                         if record.partner_id.bloque_vo and record.partner_id.debloque_exce_vo==True:
                             record.test_bloque=""'''
                 
-    @api.onchange('order_line','order_line.product_id','order_line.product_uom_qty')                 
+    '''@api.onchange('order_line','order_line.product_id','order_line.product_uom_qty')                 
     def on_change_for_bloque_ch(self):
         for sale in self:
             if sale.partner_id.blocagex_echeance_facture_charcuterie==True:                
                 if sale.partner_id.nbr_jours_decheance_charcuterie > sale.partner_id.echeance_charcuterie_par_jour:
                     partner=self.env['res.partner'].search([('id', '=', sale.partner_id.id)],limit=1)
-                    partner.write({'bloque_ch': True,'bloque': True})
+                    partner.write({'bloque_ch': True,'bloque': True})'''
                 
                 
     
@@ -576,6 +576,9 @@ class SaleOrder(models.Model):
     @api.onchange('order_line','order_line.product_id','order_line.product_uom_qty')
     def onchange_order_line_livre(self):
         for record in self:
+            if sale.partner_id.nbr_jours_decheance_charcuterie > sale.partner_id.echeance_charcuterie_par_jour:
+                partner=self.env['res.partner'].search([('id', '=', sale.partner_id.id)],limit=1)
+                partner.write({'bloque_ch': True,'bloque': True})
             record._compute_weight_total_stock_vv()
             record._compute_weight_total_stock_srg()
             record._compute_weight_total_stock_char()
