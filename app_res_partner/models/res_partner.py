7e32d73c8e67174bea071685a56648e744013555
Admin <Admin@DESKTOP-VHKQN61.mshome.net>
2021-06-22 12:39:17 +0100

    condition 2

--- app_res_partner/models/res_partner.py
+++ app_res_partner/models/res_partner.py
@@ -210,11 +210,11 @@ class ResPartner(models.Model):
                 
             if record.blocagex_limite_credit_charcuterie:
                 if record.credit_charcuterie > record.limite_credit_charcuterie:
-                    record.bloque_ch=False
-                    record.bloque=False
-                if record.credit_charcuterie < record.limite_credit_charcuterie:
                     record.bloque_ch=True
                     record.bloque=True
+                if record.credit_charcuterie < record.limite_credit_charcuterie:
+                    record.bloque_ch=False
+                    record.bloque=False
             else:
                 record.bloque_ch=False
                 record.bloque=False
@@ -223,6 +223,9 @@ class ResPartner(models.Model):
                 if record.nbr_jours_decheance_charcuterie > record.echeance_charcuterie_par_jour:
                     record.bloque_ch=True
                     record.bloque=True
+                if record.nbr_jours_decheance_charcuterie < record.echeance_charcuterie_par_jour:
+                    record.bloque_ch=False
+                    record.bloque=False
             else:
                 record.bloque_ch=False
                 record.bloque=False
