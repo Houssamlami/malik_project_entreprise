b1dfc810c324c22ad9a45df4c4d0a3fcbd93cdb8
Admin <Admin@DESKTOP-VHKQN61.mshome.net>
2021-06-29 10:21:26 +0100

    try20

--- app_res_partner/models/res_partner.py
+++ app_res_partner/models/res_partner.py
@@ -208,12 +208,10 @@ class ResPartner(models.Model):
                 record.bloque_vo=False
                 record.bloque=False
                 
-                
             if record.blocagex_limite_credit_charcuterie:
                 if record.credit_charcuterie > record.limite_credit_charcuterie:
                     record.bloque_ch=True
                     record.bloque=True
-
             else:
                 record.bloque_ch=False
                 record.bloque=False
@@ -222,7 +220,6 @@ class ResPartner(models.Model):
                 if record.nbr_jours_decheance_charcuterie > record.echeance_charcuterie_par_jour:
                     record.bloque_ch=True
                     record.bloque=True
-
             else:
                 record.bloque_ch=False
                 record.bloque=False
